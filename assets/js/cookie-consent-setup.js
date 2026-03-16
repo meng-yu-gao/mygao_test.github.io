13e8f35a3a0ee14eb8bf685199ff9f90191fa3a6
meng-yu-gao <mgao0709@gmail.com>
2026-03-16 19:13:05 +0000

    Deploying to gh-pages from @ meng-yu-gao/mygao_test.github.io@fb418c5ae6901f63af0fb7af32bcbdebbcb7e234 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://mygao.github.io/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://meng-yu-gao.github.io/mygao_test.github.io//#contact">contact us</a>.'
               }
             ]
           }
